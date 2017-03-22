e2c2fcbf91003ae26b5a932a1042eb2884196ebd
crearis <doenitz@tpzdasei.de>
2017-03-22 17:49:31 +0100

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <v-app top-toolbar>
+    <header>
+      <v-toolbar>
+        <v-toolbar-logo>Vuetify</v-toolbar-logo>
+        <v-toolbar-items>
+          <v-toolbar-item :item="item"></v-toolbar-item>
+        </v-toolbar-items>
+      </v-toolbar>
+    </header>
+
+    <main>
+      <v-content>
+        <v-container fluid>
+          <router-view></router-view>
+        </v-container>
+      </v-content>
+    </main>
+  </v-app>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        item: {
+          href: '#!',
+          text: 'Get Started'
+        }
+      }
+    }
+  }
+</script>
